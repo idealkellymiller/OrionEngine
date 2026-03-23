a606399f89d74248e42b847223da7e4606cf20e1
Kelly Miller <144192153+idealkellymiller@users.noreply.github.com>
2026-03-23 09:47:37 -0500

    Updated ImGUI submodule and Application.cpp

--- OrionEngine/engine/src/Application.cpp
+++ OrionEngine/engine/src/Application.cpp
@@ -4,7 +4,7 @@
 
 namespace Orion {
 
-//#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
 
 	Application* Application::s_Instance = nullptr;
 

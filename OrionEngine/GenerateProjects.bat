f925c1dfce913f38ad34b38ad291bfc71edf04c5
lukedholder <lukedholder@gmail.com>
2026-04-10 12:00:13 -0500

    Added physTest scene

--- OrionEngine/GenerateProjects.bat
+++ OrionEngine/GenerateProjects.bat
@@ -1,7 +1,10 @@
 @echo off
-mkdir build
+if not exist build mkdir build
 cd build
-call cmake ..
+
+cmake .. -G "Visual Studio 17 2022" -A x64
+cmake --build . --config Debug
+
 PAUSE
 
 @REM echo CMake is not installed or not found in PATH.
